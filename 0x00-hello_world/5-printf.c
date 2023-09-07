c0715b860fef9ca30a9ab5a21a241e8d11536120
Alzulal <zaloo9970@gmail.com>
2023-09-07 14:00:12 -0700

    Hello World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
  * main - A program that print a line using the printf funtion
+ * Description: print string
  *
  * Return: 0 (Success)
  */
+
 int main(void)
 {
 	priitf("with proper grammar, but the outcome is a piece of art,\n")
